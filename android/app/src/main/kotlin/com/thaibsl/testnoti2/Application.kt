22f10987ff2e05ce2b13f59a0bfe5e338be8c6f6
Thavirat <twetheam@gmail.com>
2021-01-15 02:20:56 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/thaibsl/testnoti2/Application.kt
@@ -0,0 +1,23 @@
+package com.thaibsl.testnoti2
+
+import com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+        if (registry != null) {
+            FlutterLocalNotificationsPlugin.registerWith(registry.registrarFor("com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"))
+        };
+    }
+}
---
+++ android/app/src/main/kotlin/com/thaibsl/testnoti2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thaibsl.testnoti2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
